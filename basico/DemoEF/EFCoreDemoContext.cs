fd0d2b48eb6f6dd37bba4c110b359c12113480bf
Fabiano Nalin <fnalin@msn.com>
2017-11-16 12:08:56 -0200

    Add projeto básico

---
+++ basico/DemoEF/EFCoreDemoContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoEF
+{
+    public class EFCoreDemoContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EFCoreDemoDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        public EFCoreDemoContext()
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ basico/DemoEF/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DemoEF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new EFCoreDemoContext())
+            {
+                var author = new Author
+                {
+                    FirstName = "William",
+                    LastName = "Shakespeare",
+                    Books = new List<Book>
+                    {
+                        new Book { Title = "Hamlet"},
+                        new Book { Title = "Othello" },
+                        new Book { Title = "MacBeth" }
+                    }
+                };
+                context.Add(author);
+                context.SaveChanges();
+            }
+            Console.WriteLine("Fim");
+            Console.ReadLine();
+        }
+    }
+}
