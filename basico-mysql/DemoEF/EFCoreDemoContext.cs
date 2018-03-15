b3a8a91e7c7ef40e82468ebf16b3726ba0e26d8d
Fabiano Nalin <fnalin@msn.com>
2018-03-15 11:33:23 -0300

    add mysql demo

---
+++ basico-mysql/DemoEF/EFCoreDemoContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DemoEF
+{
+    public class EFCoreDemoContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(@"Server=localhost;DataBase=Livraria;Uid=root;Pwd=123456@qwerty");
+        }
+
+        public EFCoreDemoContext()
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ basico-mysql/DemoEF/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace DemoEF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new EFCoreDemoContext())
+            {
+                //var author = new Author
+                //{
+                //    FirstName = "William",
+                //    LastName = "Shakespeare",
+                //    Books = new List<Book>
+                //    {
+                //        new Book { Title = "Hamlet"},
+                //        new Book { Title = "Othello" },
+                //        new Book { Title = "MacBeth" }
+                //    }
+                //};
+                //context.Add(author);
+                //context.SaveChanges();
+
+                context.Books.Include(a => a.Author).ToList().ForEach(l=> {
+                    Console.WriteLine(l.Title);
+                });
+
+            }
+            Console.WriteLine("Fim");
+            Console.ReadLine();
+        }
+    }
+}
